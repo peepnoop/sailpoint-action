1e8a1cb8d03dd0163ed828ea9a90ecac3a55e714
Alec Smith <asmith@tamu.edu>
2024-10-23 14:14:01 -0400

    update readme and add more api functionality

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # SailPoint Github Action
 
-This action interfaces with the SailPoint CLI to preform various action 
+> !WARNING
+> This action is still under development
+
+This action interfaces with the SailPoint APIs to allow for action to be preformed within a CI/CD pipeline.
+This repository is still in early development, methodologies and process may change as the project matures.
+
+## How to use this action
+
+To use this action add the following block of code to your GitHub workflow
+
+```yaml
+    - uses: peepnoop/sailpoint-action@v1
+      with:
+        	sailpointURL: <ENV VAR FOR URL>
+            clientID: <ENV VAR FOR CLIENT ID>
+            clientSecret: <ENV VAR FOR CLIENT SECRET>
+            action: <ACTION TO PREFORM. SEE BELOW>
+	        payload: <VALID PAYLOAD FOR THE ACTION. SEE BELOW>
+```
+
+
+## Valid actions
+
+| Action name | Valid Payload | Description |
+| --- | --- | --- |
+| create-transform | JSON file containing valid transform | Validates the payload and calls the Transforms API to create a new transform |
+| update-transform | JSON file containing an existing transform | Validates the payload and calls the Transforms API to update an existing transform |
+| delete-transform | JSON file containing an existing transform | Validates the payload and calls the Transforms API to delete an existing transform |
+| create-role | JSON file containing a valid role object | Validates the payload and calls the Roles API to generate a new role |
+
+
--- main.go
+++ main.go
@@ -13,11 +13,27 @@ import (
 
 )
 
+// ValidatorResult interface
 type ValidatorResult interface {
+	Unwrap() interface{}
 }
-func (t api_v2024.Transform) ValidatorResult() {}
-type ValidatorString string
-func (s ValidatorString) ValidatorResult() {}
+// Define a wrapper type for api_v2024.Transform
+type TransformWrapper struct {
+    Transform api_v2024.Transform
+}
+
+type RoleWrapper struct {
+	Role api_v2024.Role
+}
+
+// Implement ValidatorResult for TransformWrapper
+func (tw TransformWrapper) Unwrap() interface{} {
+    return tw.Transform
+}
+
+func (rw RoleWrapper) Unwrap() interface{} {
+    return rw.Role
+}
 
 func config(sailpointURL string,clientID string,clientSecret string) sailpoint.ClientConfiguration {
 
@@ -33,6 +49,8 @@ func config(sailpointURL string,clientID string,clientSecret string) sailpoint.C
 func validator(action string, payload string) (ValidatorResult, error){
 
 	// check the type of action to see what needs to be validated
+
+	// TRANSFORMS //
 	if action == "create-transform" {
 		// validate the payload
 
@@ -61,17 +79,74 @@ func validator(action string, payload string) (ValidatorResult, error){
 		if transform.GetName() == "" {
 			return nil,fmt.Errorf("The transform must have a name")
 		}
-		return transform,nil
+		return TransformWrapper{Transform: transform},nil
+	}
+	if action == "update-transform" {
+		var transform api_v2024.TransformRead
+
+			filepath := payload
+			if filepath != "" {
+				file, err := os.Open(filepath)
+				if err != nil {
+					return nil,err
+				}
+				defer file.Close()
+
+				err = json.NewDecoder(file).Decode(&transform)
+				if err != nil {
+					return nil,err
+				}
+			} else {
+				err := json.NewDecoder(os.Stdin).Decode(&transform)
+				if err != nil {
+					return nil,err
+				}
+			}
+
+			if transform.Id == "" {
+				return nil,fmt.Errorf("The transform must have an Id to update")
+			}
+	}
+
+	// ROLES //
+	if action == "create-role" {
+		// validate the payload
+
+		var role api_v2024.Role
+		var decoder *json.Decoder
+
+		if payload != "" {
+			file, err := os.Open(payload)
+			if err != nil {
+				return nil,err
+			}
+			defer file.Close()
+			decoder = json.NewDecoder(bufio.NewReader(file))
+		} else {
+			decoder = json.NewDecoder(bufio.NewReader(os.Stdin))
+		}
+
+		if err := decoder.Decode(&role); err != nil {
+			return nil,err
+		}
+
+		log.Debug("Filepath", "path", payload)
+		log.Debug("Role", "role", role)
+
+		if role.GetName() == "" {
+			return nil,fmt.Errorf("The role must have a name")
+		}
+		return RoleWrapper{Role: role},nil
 	}
 	return nil,fmt.Errorf("Unknown action: %s", action)
 }
 
 func main() {
-	// sailpointURL := os.Getenv("INPUT_SAILPOINT_URL")
-	// clientID := os.Getenv("INPUT_CLIENT_ID")
-	// clientSecret := os.Getenv("INPUT_CLIENT_SECRET")
-	// action := os.Getenv("INPUT_ACTION")
-	// payload := os.Getenv("INPUT_PAYLOAD")
+	sailpointURL := os.Getenv("INPUT_SAILPOINT_URL")
+	clientID := os.Getenv("INPUT_CLIENT_ID")
+	clientSecret := os.Getenv("INPUT_CLIENT_SECRET")
+	action := os.Getenv("INPUT_ACTION")
+	payload := os.Getenv("INPUT_PAYLOAD")
 
 
 	// TESTING VALUES REMOVE BEFORE PUSHING
@@ -83,50 +158,128 @@ func main() {
 	apiClient := sailpoint.NewAPIClient(configuration)
 	configuration.HTTPClient.RetryMax = 10
 
-	// get the list of sources for validation if necessary
-	sources, _, err := apiClient.V2024.SourcesAPI.ListSources(ctx).Execute()
-	source_info := make(map[string]string)
 
-	if err != nil {
-		fmt.Println(err)
-	}
+	// TESTING BLOCK NEEDS //
+	// TO BE MOVED TO VALIDATION //
 
-	// Loop sources and get the id for each source
-	for _, source := range sources {
-		// append the id and name to a map of source ids and names
-		source_info[source.GetId()] = source.Name
+
+	// // get the list of sources for validation if necessary
+	// sources, _, err := apiClient.V2024.SourcesAPI.ListSources(ctx).Execute()
+	// source_info := make(map[string]string)
+
+	// if err != nil {
+	// 	log.Error("Failed to get sources", "err", err)
+	// }
+
+	// // Loop sources and get the id for each source
+	// for _, source := range sources {
+	// 	// append the id and name to a map of source ids and names
+	// 	source_info[source.GetId()] = source.Name
 		
-	}
+	// }
 
-	// DEBUG
-	fmt.Println(source_info)
-	// END DEBUG
+	// // DEBUG
+	// log.Debug("Sources", "sources", source_info)
+	// // END DEBUG
+
+	// END TESTING BLOCK //
 
 
 	// Transforms
 	if action == "create-transform" {
 
 		// pass the payload to the validate function to ensure the transform is valid
-		transform,err := validator(action,payload)
+		result,err := validator(action,payload)
 
-		if transform != nil {
-			fmt.Println("Transform is invalid")
-			fmt.Println(transform)
+		if err != nil {
+			log.Error("Failed to validate transform", "err", err)
 		}
+
+		// Perform type assertion
+		transformWrapper, ok := result.(TransformWrapper)
+		if !ok {
+			log.Fatal("Result is not of type api_v2024.Transform")
+		}
+
+		transform := transformWrapper.Transform
+
+		// get the source id from the transform
 
 		// actually create the transform
-		transformObj, resp, err := apiClient.V2024.TransformsAPI.CreateTransform(ctx).Transform(transform).Execute()
+		transformObj, _, err := apiClient.V2024.TransformsAPI.CreateTransform(ctx).Transform(transform).Execute()
 		if err != nil {
-			fmt.Println(resp,err)
+			log.Error("Failed to create transform", "err", err)
 		}
-		fmt.Print("Transform created successfully")
-		fmt.Print(transformObj.Id)
+		log.Info("Transform created successfully", "transform", transformObj.Id)
 
+	}
 
+	if action == "update-transform"{
+		// pass the payload to the validate function to ensure the transform is valid
+		result,err := validator(action,payload)
 
+		if err != nil {
+			log.Error("Failed to validate transform", "err", err)
+		}
+
+		// Perform type assertion
+		transformWrapper, ok := result.(TransformWrapper)
+		if !ok {
+			log.Fatal("Result is not of type api_v2024.Transform")
+		}
+
+		transform := transformWrapper.Transform
+
+		updateTransform := api_v2024.Transform{Attributes: transform.Attributes, Type: transform.Type, Name: transform.Name}
+
+		// actually create the transform
+		transformObj, _, err := apiClient.V2024.TransformsAPI.UpdateTransform(ctx,transform.Id).Transform(updateTransform).Execute()
+		if err != nil {
+			log.Error("Failed to update transform", "err", err)
+		}
+		log.Info("Transform updated successfully", "transform", transformObj.Id)
+
+	}
+
+	if action == "delete-transform" {
+		// attempt to delete the transform
+		resp, err := apiClient.V2024.TransformsAPI.DeleteTransform(ctx, payload).Execute()
+
+		if err != nil {
+			log.Error("Failed to delete transform", "err", err)
+		}
+
+		log.Info("Transform deleted successfully", "transform", resp.StatusCode)
 	}
 
 	// Sources
 
 	// Identity Profiles
+
+	// Roles
+	if action == "create-role" {
+		// pass the payload to the validate function to ensure the json is valid
+		result,err := validator(action,payload)
+
+		if err != nil {
+			log.Error("Failed to validate Role json", "err", err)
+		}
+
+		// Perform type assertion
+		roleWrapper, ok := result.(RoleWrapper)
+		if !ok {
+			log.Fatal("Result is not of type api_v2024.Role")
+		}
+
+		role := roleWrapper.Role
+
+		// create the role
+		roleObj, _, err := apiClient.V2024.RolesAPI.CreateRole(ctx).Role(role).Execute()
+		if err != nil {
+			log.Error("Failed to create role", "err", err)
+		}
+		log.Info("Transform created successfully", "role", roleObj.Id)
+	} else {
+		log.Error("Unknown action", "action", action)
+	}
 }
